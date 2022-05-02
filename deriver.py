abea354db61e5f55f1e91f39d139559d658e7cc3
nmcs4102 <38853987+nmcs4102@users.noreply.github.com>
2022-05-02 21:03:37 +0200

    Final version of deriver
    Final version of deriver, seems to be completely fixed,
    Fixed the last bug I found where the negatives of sin(x) and other functions wouldn't derive correctly

--- deriver.py
+++ deriver.py
@@ -3,139 +3,139 @@ def derive(exp):
     comps = complex_functions_parse(exp, match(exp))
     opers = operators(exp, match(exp), comps)
     derivatives = []
-    # print("Derive: {}".format(exp))
+    if (exp[0] == "(" and exp[-1] == ")") and (match(exp)[0] == len(exp)-1):
+        return derive(exp[1:-1])
     if len(splitted) == 1:
-        # Deriving Constants
+        le_finale = ""
+        le_inverse = "-"
+        if splitted[0][0] == "-":
+            le_finale = "-"
+            le_inverse = ""
         if "x" not in exp:
             return 0
-        if exp in ["x", "(x)"]:
-            return 1
+        if exp in ["x", "(x)", "-x"]:
+            return "{}1".format(le_finale)
         elif exp[-1] == "x":
             try:
                 c = int(exp[:-1])
                 return c
             except:
                 pass
-        # print("Deriv Inside:")
-        # print(opers, comps)
-        ld = []
-        # What we know about stuff that made it this far: 1. Is connected to X 2. wkrn
         for i in range(len(exp)):
             if i in opers:
-                # print("True")
                 ls = exp[opers[i][1]:opers[i][5]]
                 rs = exp[opers[i][5]+1:opers[i][2]+1]
-                if len(match(exp)) == 2:
-                    if ls[0] == "(" and ls[-1] == ")":
-                        if ls[0] == "(":
-                            ls = ls[1:]
-                        if ls[-1] == ")":
-                            ls = ls[:-1]
-                    if rs[0] == "(" and rs[-1] == ")":
-                        if rs[0] == "(":
-                            rs = rs[1:]
-                        if rs[-1] == ")":
-                            rs = rs[:-1]
-                # remove brackets from start and end
+                if ls != "" and rs != "":
+                    if len(match(exp)) == 2:
+                        if ls[0] == "(" and ls[-1] == ")":
+                            if ls[0] == "(":
+                                ls = ls[1:]
+                            if ls[-1] == ")":
+                                ls = ls[:-1]
+                        if rs[0] == "(" and rs[-1] == ")":
+                            if rs[0] == "(":
+                                rs = rs[1:]
+                            if rs[-1] == ")":
+                                rs = rs[:-1]
                 if opers[i][0] == "^":
-                    # Left Side, Right Side
-                    # print(ls, rs)
                     if ls == "x":
                         try:
                             power = int(rs)
-                            return "{0}*x^({1})".format(power, power-1)
+                            return "{0}({1})*x^({2})".format(le_finale, power, power-1)
                         except:
-                            return "({0})^({1})*({2})".format(ls, rs, derive("ln({0})*({1})".format(ls, rs)))
-                    if ls == "e":
+                            return "{0}({1})^({2})*({3})".format(le_finale, ls, rs, derive("ln({0})*({1})".format(ls, rs)))
+                    elif ls == "e":
                         if "x" in rs:
                             if rs == "x":
-                                return "e^x"
+                                return "{}e^x".format(le_finale)
                             else:
-                                return "({0})*e^({1})".format(derive(rs[1:-1]), rs)
+                                return "{0}({1})*e^({2})".format(le_finale, derive(rs), rs)
                     else:
                         try:
                             c = int(ls)
-                            return "ln({0})*({1})*{2}".format(c, exp, derive(rs))
+                            return "{0}ln({1})*({2})*{3}".format(le_finale, c, exp, derive(rs))
                         except:
                             if "x" in rs or "x" in ls:
                                 if "x" in rs and "x" in ls:
-                                    return "({0})^({1})*({2})".format(ls, rs, derive("ln({0})*({1})".format(ls, rs)))
+                                    return "{0}({1})^({2})*({3})".format(le_finale, ls, rs, derive("ln({0})*({1})".format(ls, rs)))
                                 elif "x" in ls:
                                     try:
                                         power = int(rs)
-                                        return "{0}*({1})^({2})*{3}".format(power, ls, power-1, derive(ls))
+                                        return "{0}({1})*({2})^({3})*{4}".format(le_finale, power, ls, power-1, derive(ls))
                                     except:
-                                        pass
-                                # Not coded: constant to the power of function (might be already performed by line 57?)
+                                        # Type J+ is a fractional power with no weird things going on (eg. ...^(1/3))
+                                        f_parts = list(map(int, rs[1:-1].split("/")))
+                                        f_new = f_parts[0]-f_parts[1]
+                                        return "{0}({1}/{2})*({3})^({4}/{5})*({6})".format(le_finale, f_parts[0], f_parts[1], ls,
+                                                                                        f_new, f_parts[1], derive(ls))
                 if opers[i][0] == "*":
-                    # print("*", exp)
                     if "x" in ls and "x" in rs:
-                        return "{0}*{1}+{2}*{3}".format(ls, derive(rs), derive(ls), rs)
+                        return "{0}({1})*({2})+({3})*({4})".format(le_finale, ls, derive(rs), derive(ls), rs)
                     elif "x" in rs:
-                        return "{0}*{1}".format(ls, derive(rs))
+                        return "{0}({1})*({2})".format(le_finale, ls, derive(rs))
                     elif "x" in ls:
-                        return "{0}*{1}".format(rs, derive(ls))
+                        return "{0}({1})*({2})".format(le_finale, rs, derive(ls))
                     else:
-                        return "{0}{1}".format(str(eval(exp[opers[i][1]:opers[i][2]+1])), exp[opers[i][2]+1:])
+                        return "{0}{1}{2}".format(le_finale, str(eval(exp[opers[i][1]:opers[i][2]+1])), exp[opers[i][2]+1:])
                 if opers[i][0] == "/":
                     if "x" in ls and "x" in rs:
-                        return "({0}*{1}-{2}*{3})/({4})^2".format(derive(ls), rs, ls, derive(rs), rs)
-                    # fix these babies
+                        return "{0}(({1})*({2})-({3})*({4}))/({5})^2".format(le_finale, derive(ls), rs, ls, derive(rs), rs)
+                    # This baby doesn't have le_finale
                     elif "x" in rs:
-                        return "-{0}*{1}/({2})^2".format(ls, derive(rs), rs)
+                        return "{0}(({1})*({2}))/({3})^2".format(le_inverse, ls, derive(rs), rs)
                     elif "x" in ls:
-                        return "{0}/{1}".format(derive(ls), rs)
+                        return "{0}({1})/({2})".format(le_finale, derive(ls), rs)
                     else:
-                        return "{0}*{1}".format(str(eval(exp[opers[i][1]:opers[i][2]+1])), exp[opers[i][2]+1:])
+                        return "{0}({1})*({2})".format(le_finale, str(eval(exp[opers[i][1]:opers[i][2]+1])), exp[opers[i][2]+1:])
             if i in comps:
                 inside = comps[i][1][1:-1]
                 if "x" not in inside:
-                    return "{0}({1})".format(comps[i][0], inside)
+                    return "{0}{1}({2})".format(le_finale, comps[i][0], inside)
                 if comps[i][0] == "sin":
-                    return "cos({0})*{1}".format(inside, derive(inside))
+                    return "{0}cos({1})*({2})".format(le_finale, inside, derive(inside))
                 elif comps[i][0] == "cos":
-                    return "-sin({0})*{1}".format(inside, derive(inside))
+                    return "{0}sin({1})*({2})".format(le_inverse, inside, derive(inside))
                 elif comps[i][0] == "ln":
-                    return "{0}*(1/({1}))".format(derive(inside), inside)
+                    return "{0}{1}*(1/({2}))".format(le_finale, derive(inside), inside)
                 elif comps[i][0] == "tan":
-                    return "((1/((cos({0}))^2))*{1}".format(inside, derive(inside))
+                    return "{0}((1/((cos({1}))^2))*{2}".format(le_finale, inside, derive(inside))
                 elif comps[i][0] == "cot":
-                    return "((1/((sin({0}))^2))*{1}".format(inside, derive(inside))
+                    return "{0}((1/((sin({1}))^2))*{2}".format(le_finale, inside, derive(inside))
                 elif comps[i][0] == "arcsin":
-                    return "({0})/((1-({1})^2)^(1/2))".format(derive(inside), inside)
+                    return "{0}({1})/((1-({2})^2)^(1/2))".format(le_finale,derive(inside), inside)
                 elif comps[i][0] == "arccos":
-                    return "-({0})/((1-({1})^2)^(1/2))".format(derive(inside), inside)
+                    return "{0}({1})/((1-({2})^2)^(1/2))".format(le_inverse, derive(inside), inside)
                 elif comps[i][0] == "arctan":
-                    return "({0})/(({1})^2+1)".format(derive(inside), inside)
+                    return "{0}({1})/(({2})^2+1)".format(le_finale, derive(inside), inside)
+                elif comps[i][0] == "log":
+                    args = inside.split(",")
+                    return "{0}{1}".format(le_finale, derive("(ln({0}))/(ln({1}))".format(args[1], args[0])))
     else:
-        # I suspect something is wrong around here
         for k in range(len(splitted)):
             derivatives.append(derive(splitted[k][1]))
-        res = "({0}".format(derivatives[0])
+        if splitted[0][0] == "+":
+            splitted[0][0] = ""
+        res = "({0}{1}".format(splitted[0][0], derivatives[0])
         for k in range(1, len(derivatives)):
             if derivatives[k] != 0:
                 res = "{0}{1}{2}".format(res, splitted[k][0], derivatives[k])
         derivatives = "{0})".format(res)
     if derivatives == []:
         print("nullres error:", exp)
+        raise NotImplemented
     return derivatives
 
 
 def process(exp):
-    print(exp)
     return derive(exp)
 
 
-# The purpose of this is to create a dict like the one made for complex functions
 def operators(exp, brackets, comp_fs):
-    # [index in i]: [operator, start, end, start_type, end_type, operator index]
     operations = {}
     if "^" in exp:
         for i in range(len(exp)-1, -1, -1):
             if exp[i] == "^":
-                # Indices where operations start and end
                 op_start, op_end, oet, ost = 0, 0, 0, 0
-                # figure out what's on the right
                 if i+1 in operations:
                     op_end = operations[i+1][2]
                     oet = "O"
@@ -156,7 +156,6 @@ def operators(exp, brackets, comp_fs):
                         oet = "X"
                     else:
                         oet = "N"
-                # Establish what's on the left
                 if i-1 in operations:
                     op_start = operations[i-1][1]
                     ost = "O"
@@ -182,9 +181,7 @@ def operators(exp, brackets, comp_fs):
     if "/" in exp:
         for i in range(len(exp)-1, -1, -1):
             if exp[i] == "/":
-                # Indices where operations start and end
                 op_start, op_end, oet, ost = 0, 0, 0, 0
-                # figure out what's on the right
                 if i+1 in operations:
                     op_end = operations[i+1][2]
                     oet = "O"
@@ -205,7 +202,6 @@ def operators(exp, brackets, comp_fs):
                         oet = "X"
                     else:
                         oet = "N"
-                # Establish what's on the left
                 if i-1 in operations:
                     op_start = operations[i-1][1]
                     ost = "O"
@@ -231,9 +227,7 @@ def operators(exp, brackets, comp_fs):
     if "*" in exp:
         for i in range(len(exp)-1, -1, -1):
             if exp[i] == "*":
-                # Indices where operations start and end
                 op_start, op_end, oet, ost = 0, 0, 0, 0
-                # figure out what's on the right
                 if i+1 in operations:
                     op_end = operations[i+1][2]
                     oet = "O"
@@ -254,7 +248,6 @@ def operators(exp, brackets, comp_fs):
                         oet = "X"
                     else:
                         oet = "N"
-                # Establish what's on the left
                 if i-1 in operations:
                     op_start = operations[i-1][1]
                     ost = "O"
@@ -280,9 +273,7 @@ def operators(exp, brackets, comp_fs):
     if "+" in exp:
         for i in range(len(exp)-1, -1, -1):
             if exp[i] == "+":
-                # Indices where operations start and end
                 op_start, op_end, oet, ost = 0, 0, 0, 0
-                # figure out what's on the right
                 if i+1 in operations:
                     op_end = operations[i+1][2]
                     oet = "O"
@@ -303,7 +294,6 @@ def operators(exp, brackets, comp_fs):
                         oet = "X"
                     else:
                         oet = "N"
-                # Establish what's on the left
                 if i-1 in operations:
                     op_start = operations[i-1][1]
                     ost = "O"
@@ -326,13 +316,10 @@ def operators(exp, brackets, comp_fs):
                         ost = "N"
                 operations[op_start] = ["+", op_start, op_end, ost, oet, i]
                 operations[op_end] = ["+", op_start, op_end, ost, oet, i]
-    # logic for "-" when first thing in a function
     if "-" in exp:
         for i in range(len(exp)-1, -1, -1):
             if exp[i] == "-":
-                # Indices where operations start and end
                 op_start, op_end, oet, ost = 0, 0, 0, 0
-                # figure out what's on the right
                 if i+1 in operations:
                     op_end = operations[i+1][2]
                     oet = "O"
@@ -353,7 +340,6 @@ def operators(exp, brackets, comp_fs):
                         oet = "X"
                     else:
                         oet = "N"
-                # Establish what's on the left
                 if i-1 in operations:
                     op_start = operations[i-1][1]
                     ost = "O"
@@ -384,13 +370,11 @@ def split_deriv(exp, brackets_dict):
     po = 0
     prev_ind = 0
     splitted = []
-    # print(exp)
     if len(brackets_dict) == 2:
         if exp[0] == "(" and exp[-1] == ")":
             exp = exp[1:-1]
             brackets_dict = match(exp)
     while i < len(exp):
-        # print(i, exp[i])
         if exp[i] in ["+", "-"]:
             cur = exp[prev_ind:i]
             if cur != "":
@@ -406,34 +390,25 @@ def split_deriv(exp, brackets_dict):
         if i in brackets_dict:
             i = brackets_dict[i]
         i += 1
-    # append last thing
     if po == 0:
         splitted.append(["+", exp[prev_ind:i]])
     else:
         splitted.append(["-", exp[prev_ind:i]])
-    # print(splitted)
     return splitted
 
 
-# parses trig functions as well as logarithms, returned dict gives you the function that starts/ends at certain index
 def complex_functions_parse(exp, brackets_dict):
-    processed_fs_dict = {}
-    i = 0
+    p_dict, res, i = brackets_dict, {}, 0
     while i < len(exp):
-        if exp[i] in ["c", "s", "l", "t"]:
-            cur = exp[i:i+3]
-            if cur[-1] in ["s", "n", "t", "g"]:
-                args = exp[i+3:brackets_dict[i+3]+1]
-                processed_fs_dict[i] = [cur, args, brackets_dict[i+3]]
-                processed_fs_dict[brackets_dict[i+3]] = [cur, args, i]
-                i += 2
-            else:
-                args = exp[i+2:brackets_dict[i+2]+1]
-                processed_fs_dict[i] = [cur[:-1], args, brackets_dict[i+2]]
-                processed_fs_dict[brackets_dict[i+2]] = [cur[:-1], args, i]
-                i += 1
+        if exp[i] in ["a", "c", "l", "s", "t"]:
+            for k in range(2, 7):
+                if exp[i:i + k] in ["sin", "cos", "tan", "ln", "log", "arcsin", "arccos", "arctan"]:
+                    res[i] = [exp[i:(i + k)], exp[i + k:p_dict[i + k]+1], p_dict[i + k]]
+                    res[p_dict[i + k]] = [exp[i:(i + k)], exp[i + k:p_dict[i + k]+1], i]
+                    i += k
+                    break
         i += 1
-    return processed_fs_dict
+    return res
 
 
 def match(exp):
@@ -449,29 +424,9 @@ def match(exp):
     return res
 
 
-problems_old = ["-ln(x+1)*(32313x-132131)/((sin(1313x)+42-cos(49x)*327x)+8x)", "sin(323x)*cos(2986x)+sin(32x)-cos(69x)",
-                "sin(x^32)", "ln(x)+34*cos(x+1)", "x^8", "ln(2x+1)", "ln(x^2+4x+2)", "8+9-132*34563",
-                "sin(89*x)/cos(78*x^2+2x)", "34*84*34/(sin(x)-1)", "54*e^(x^2+2x)", "x^(1/2)",
-                "x+x^(1/2)+x^(1/3)+x^(1/5)", "x*ln(x)", "2*sin(x)/(sin(x)-cos(x))", "x^2*e^x*sin(x)", "625*x^10",
-                "e^(x^(1/2))*(x^2-1)^(1/2)", "e^(x^(1/2))*(x^2-1)^(1/2)", "x^4*ln(x)", "2^x*4*cos(x)"]
-# Don't give problems where xes are not multiplied together normally
-problems_mvp = ["-5*x^8+(2/3)*x^(-2)+(1/5)*x-21", "6*x^(1/3)-3*x^(2/3)-(6/5)*x^(-5)+2*x^(-2)",
-                "-3*x^3+(x^(5/4))*(2*x^3)", "(x^5+3x)*sin(x)", "2^x*4*cos(x)",
-                "(2*sin(x)+5*x^(1/3))/(5*3^x)", "3*x^(-3)*ln(x)*3^x", "sin(x^4)", "cos(2^x)",
-                "(x^5-2*x^2+3*x+5)^11", "sin(5*x^2)*4^x", "3^(x^3-4*x+2)*5^(5*x+3)",
-                "(5*x^4-x^2+10*x)^(1/3)+(2*x+3)^10*cos(x^2)", "(sin(5*x+1))^8", "e^((cos(x))^3)",
-                "((2^(x^3))+5*x)^(1/2)/5", "(sin(3^(2*x^2+2))^2", "x^3/(ln(x^2))", "sin(x)/cos(x)",
-                "sin(e^x)+cos(ln(x))"]
-problem_ids = [1, 2, 3, 4, 5, 8, 9, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 23, "B", "D10"]
-# 1 C, 2 C, 3 F (- instead of +), 4 C, 5 C, 8 F (holy heck), 9 F (close), 11 C, 12 C,
-# 14 C, 15 C, 16 F (parsing is bad), 17 F (no), 18 C, 19 C, 20 F (massive),
-# 21 C, 22  F (Anomaly) "x^2*e^(-x^2)", 23 C,   B C, D10 C
-#for i in range(len(problems_mvp)):
-    # print(problem_ids[i], "Derivative:", process(problems_mvp[i]))
-print("Derivative:", process("(2*sin(x)+5*x^(1/3))*(5*3^x)"))
-# print("Derivative:", process("x^3/(ln(x^2))"))
-# print(process(input("")))
-# This baby does 12.5/18 MVP
-# sin cos ln, e^x
 while True:
-    print("Derivative:", process(input("Expression: ")))
+    cur = input("Expression: ")
+    try:
+        print("Derivative:", process(cur))
+    except:
+        print("An error occured during the deriving process")
